4446021a3f279617a1b9d84c06ba0d493eb48bf9
mohamed eskandrane <93823327+mohamed-eskandrane@users.noreply.github.com>
2025-02-19 13:13:38 +0300

    Update Node2.js

--- Node2.js
+++ Node2.js
@@ -17,3 +17,30 @@ http.createServer(function (req, res) {
     return res.end();
   }
 }).listen(8080);
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hanisheets89:PBZicCAu2xhLOvuY@cluster0.9oq07.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
